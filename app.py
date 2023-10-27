f2335132439e227c68bbed3b6c9b2f62ee198961
chigzzer <42282981+Chigzzer@users.noreply.github.com>
2023-10-27 10:16:04 +0100

    Added bingo Names

--- app.py
+++ app.py
@@ -7,21 +7,23 @@ app = Flask(__name__)
 app.config['Debug'] = True
 
 
-numbers_chosen = []
-bingo_bank = [i for i in range(20)]
 
-def get_number():
-    number = random.randint(0, len(bingo_bank)-1)
+
+numbers_chosen = []
+
+def get_number(length):
+    number = random.randint(0, length-1)
     while number in numbers_chosen:
-        number = random.randint(0, len(bingo_bank) -1)
+        number = random.randint(0, length -1)
     numbers_chosen.append(number)
     return number
 
 def generate_card():
+    bingo_bank = open("bingo-texts.txt").read().splitlines()
     bingo_card_data = []
     numbers_chosen.clear()
     for i in range(9):
-        index = get_number()
+        index = get_number(len(bingo_bank))
         bingo_card_data.append(bingo_bank[index])
     print(bingo_card_data)
     return bingo_card_data
